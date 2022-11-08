5ae34752f8b0bff810bd21d60a142f0c086c5c45
AyaanZaidi <103330931+AyaanZaidi@users.noreply.github.com>
2022-11-09 02:08:52 +0500

    ayaan js assignment 3
    ayaan js assignment 3

---
+++ app.js
@@ -0,0 +1,33 @@
+// chapter: 1.
+var that_array = ["syed" , "ayaan" , "hussain" , "zaidi"];
+console.log(that_array)
+// chapter: 1 complete.
+
+// chapter: 2.
+var cities_pakistan = ["karachi" , "islamiat" , "lahore" , "pishawar"];
+cities_pakistan.push("KPK")
+console.log(cities_pakistan)
+var cities_pakistan1 = ["karachi" , "islamiat" , "lahore" , "pishawar"];
+cities_pakistan1.pop()
+console.log(cities_pakistan1)
+// chapter: 2 complete.
+
+// chapter: 3.
+var world_cuntry = ["india" , "pakistan" , "newzealand" , "england"];
+world_cuntry.shift()
+console.log(world_cuntry)
+var world_cuntry1 = ["india" , "pakistan" , "newzealand" , "england"];
+world_cuntry1.unshift("australia")
+console.log(world_cuntry1)
+var world_cuntry2 = ["india" , "pakistan" , "newzealand" , "england"];
+world_cuntry2.splice(3, 0 , "australia");
+console.log(world_cuntry2)
+var final = world_cuntry2.slice(1, 3);
+console.log(final)
+// chapter: 3 complete.
+
+// chapter: 4.
+for(var g = 0 ; g <= 30 ; g++){
+console.log(g)
+}
+// chapter: 4 complete.
